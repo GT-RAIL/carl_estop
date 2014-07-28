6e17470fe0084c7e1ac94ea1a2ab529df95ae843
Chris Dunkers <cmdunkers@gmail.com>
2014-07-28 15:11:36 -0400

    added initial code

---
+++ include/carl_estop/carl_estop_node.h
@@ -0,0 +1,73 @@
+/*
+ * \carl_estop_node.h
+ * \allows the stopping of the segway base without losing power to peripherals
+ *
+ * carl_estop_node creates a ROS node that allows the stopping of the robot after a specified amount of time.
+ * This node listens to a /carl_estop topic
+ * and sends messages to the /move_base actionlib to stop the current execution of a goal.
+ *
+ * \author Russell Toris, WPI - rctoris@wpi.edu *
+ * \author Chris Dunkers, WPI - spkordell@wpi.edu
+ * \date July 24, 2014
+ */
+
+#ifndef CARL_ESTOP_TELEOP_H_
+#define CARL_ESTOP_TELEOP_H_
+
+#include "ros/ros.h"
+#include "std_msgs/Empty.h"
+#include <actionlib/client/simple_action_client.h>
+#include <move_base_msgs/MoveBaseAction.h>
+
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> ActionClient;
+
+class carl_estop
+{
+  public:
+    carl_estop();
+    double get_frequency();
+    /*
+     * estop function to check the time difference to see if any goal position in move_base should be cancelled
+     *
+     * \param msg the empty message
+     */
+    void estop(void);
+
+  private:
+	//main node handle
+	ros::NodeHandle node;
+
+    // the ros subscriber
+    ros::Subscriber estop_sub;
+
+    //variables for the parameter values to be stored in
+    double stop_time_delay;
+    double check_frequency;
+    ros::Time last_receive;
+
+    //variable used to make sure robot only says the connection lost once
+    bool spoke;
+
+    // A handle for the move_base action client thread
+    ActionClient* actionClient;  
+
+    /*
+     * carl_estop topic callback function.
+     *
+     * \param msg the empty message
+     */
+    void update_time(const std_msgs::Empty::ConstPtr& msg);
+
+    
+};
+
+/*!
+ * Creates and runs the carl_estop_node.
+ *
+ * \param argc argument count that is passed to ros::init
+ * \param argv arguments that are passed to ros::init
+ * \return 0 
+ */
+int main(int argc, char **argv);
+
+#endif
